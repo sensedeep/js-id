5e46373961c6b9cbc65332f025e8ba6224a70a38
Embedthis Development <dev@embedthis.com>
2016-10-24 09:24:50 -0700

    DEV

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
 /*
-    node-idgen -- Create uuid and snowflake IDs
+    id.js -- Create tokens and ids
  */
 
-export default class Idgen {
-    constructor() {
-        let datacenter = 1
-        let worker = 1
-        this.idgen = {
-            epoch: 1455599858456,           // 2-15-16
-            seq: 0,
-            last: 0,
-            id: ((datacenter & 0xFF) << 24 | (worker & 0xFF)) << 16
-        }
-    }
+import * as Cluster from 'cluster'
+import * as Crypto from 'crypto'
 
-    static uuid() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
-            return v.toString(16)
+var epoch = 1455599858456           // 2-15-16
+var seq = 0
+var last = 0
+
+export class Id {
+
+    /* 
+        Get a random token. Count is the number of random bytes 
+     */
+    static async token(count, encoding = 'base64') {
+        return new Promise(function (resolve, reject) {
+            Crypto.randomBytes(count, function(err, buf) {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(buf.toString('base64'))
+                }
+            })
         })
     }
 
-    static snowflake() {
-        let idgen = this.idgen
+    /*
+        Get a human readable ID. Output in hex. Size is the number of characters in the ID.
+     */
+    static async uid(size) {
+        return new Promise(function (resolve, reject) {
+            let count = parseInt(size / 6 * 2.5)
+            Crypto.randomBytes(count, function(err, buf) {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(buf.toString('hex').match(/(.....)/g).join('-'))
+                }
+            })
+        })
+    }
+
+    /*
+        Get an ID unique over a single data center
+     */
+    static did(datacenter) {
         let buf = new Buffer(8)
-        let time = Date.now() - idgen.epoch
+        let time = Date.getTime() - gen.epoch
+        let worker = Cluster.isWorker ? Cluster.worker.id : 0
+        let id = ((this.datacenter & 0xFF) << 24 | (worker & 0xFF)) << 16
         buf.fill(0)
-        idgen.seq = (time == idgen.last) ? ((idgen.seq + 1) & 0xFFFF) : 0
-        idgen.last = time
-        /*
-            32 (time - epoch) | 8 datacenter | 8 worker | 16 seqno
-         */
-        buf.writeUInt32BE(1 << 30 | idgen.id | idgen.seq)
+        Id.seq = (time == Id.last) ? ((Id.seq + 1) & 0xFFFF) : 0
+        Id.last = time
+        buf.writeUInt32BE(1 << 30 | Id.id | Id.seq)
         buf.writeUInt32BE(time, 4)
         return buf.toString('hex')
     }
